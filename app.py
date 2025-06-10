1a826065a44161ee2c3247c5736f6fee87035896
opipec <o.castrog@uniandes.edu.co>
2025-06-09 20:13:56 -0500

    Complete load_data function to read and preprocess energy data from CSV

--- app.py
+++ app.py
@@ -21,7 +21,11 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    df = pd.read_csv("datos_energia.csv")
+    df["time"] = pd.to_datetime(df["time"])
+    df.set_index("time", inplace=True)
+    #print(df.head())
+    return df 
     
 
 # Cargar datos
